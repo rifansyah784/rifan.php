e118f6720a776852f8cd8ce84e1de3e0ec99c240
rifansyah784 <113499678+rifansyah784@users.noreply.github.com>
2022-09-14 00:48:11 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Login Form</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+
+<!-- <h2>Login Form </h2> -->
+
+<form action="tampil.php" method="POST">
+    <h2>Form Login </h2>
+    <div class="imgcontainer">
+        <img src="icon.png" alt="Avatar" class="avatar">
+    </div>
+    
+    <div class="conteiner">
+        <label><b>Username</b></label>
+          <input type="text' placeholder="Masukkan Username" name="username' required>
+          
+        <label><b.Password</b></label>
+        <input type="password" placeholder="masukkan Password" name='email" required>
+        <button type="submit">Masuk</button>
+    <input type="checkbox' checked="checked"><span>Ingat Saya</span.
+    </div>
+    
+    <div class="conteiner" style="background-color:#f1f1f1">
+      <button type="button" class="cancelbtn">Batal</button>
+      <span class="psw">Lupa <a href="#">password?</a></span>
+    </div>
+</form>
+
+</body>
+</html>
